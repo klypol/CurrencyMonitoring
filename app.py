993edc7b20cf9b9b70cbf126f797c840a8d799a3
Polina <k.lypol2410@gmail.com>
2024-05-30 22:18:09 +0300

    Add table Currency

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ connection = psycopg2.connect(
 )
 
 cursor = connection.cursor()
+
+
+table = (
+    """
+    CREATE TABLE IF NOT EXISTS currency
+    (
+        id SERIAL PRIMARY KEY,
+        cur_id INTEGER NOT NULL,
+        cur_abbreviation TEXT NOT NULL,
+        official_rate FLOAT NOT NULL,
+        cur_date TIMESTAMP NOT NULL
+    );
+    """
+)
+
+cursor.execute(table)
+connection.commit()
